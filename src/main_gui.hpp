826f2e03fbebe22ecfa7bc770488af7bc4c86985
peterem <emanuel.peter@hotmail.com>
2020-06-17 18:35:24 +0200

    Notify and recolor MapArea if PaletteArea has updates

--- src/main_gui.hpp
+++ src/main_gui.hpp
@@ -1,65 +1,18 @@
 #include "../evp/gui.hpp"
 #include "../evp/voronoi.hpp"
 
-typedef int CInfo;
-
-class MapArea : public evp::GUI::Area{
-public:
-  MapArea(evp::GUI::Area* const parent,
-          const int nPoints,
-	  const float dx,
-	  const float dy
-	 )  : Area("mapArea",parent,0,0,dx,dy) {
-     vmap = new evp::VoronoiMap<CInfo>(10000,dx,dy);
-  }
-  virtual void draw(const float px,const float py, sf::RenderTarget &target, const float pscale) {
-    float gx = x_+px*pscale;
-    float gy = y_+py*pscale;
-    DrawRect(gx, gy, dx_*pscale, dy_*pscale, target, bgColor_);
-    vmap->draw(gx,gy,zoom_,target);
-  }
-  void zoomIs(const float z) {
-    zoom_ = std::min(1e2f, std::max(1e-2f,z));
-    sizeIs(vmap->spread_x*zoom_, vmap->spread_y*zoom_);
-    evp::GUI::ScrollArea* a = dynamic_cast<evp::GUI::ScrollArea*>(parent()->parent());
-    a->adjustChildren();// make sure scrollers adjust
-  }
-  virtual void onMouseWheelScrolled(const float dx, const float dy, const float x, const float y) {
-    evp::GUI::ScrollArea* a = dynamic_cast<evp::GUI::ScrollArea*>(parent()->parent());
-    
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)
-     ||sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RControl)) {
-      float gx = globalX();
-      float gy = globalY();
-      float oldZ = zoom_;
-      zoomIs(pow(1.1,dy) * zoom_);
-      a->adjustChildren();// make sure scrollers adjust
-      
-      float realZF = zoom_/oldZ;
-      a->doScroll(
-		  (x-gx)*(realZF-1),
-		  (y-gy)*(realZF-1)
-		  );// take zoom correction, to warp around mouse
-      
-    } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift)
-     ||sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RShift)) {
-      a->doScroll(-dy*10,-dx*10);// invert scroll axes
-    } else {
-      a->doScroll(-dx*10,-dy*10);
-    }
-  }
-private:
-  evp::VoronoiMap<CInfo>* vmap;
-  float zoom_=1.0;
-  bool hasData_=false;
+struct CInfo{
+   int paletteId = 0;
+   float colFac = 0;
 };
-
 
 class PaletteArea : public evp::GUI::Area{
 public:
   PaletteArea(evp::GUI::Area* const parent)
 	  : Area("paletteArea",parent,0,0,200,400) {
     colorIs(evp::Color(0.05,0.05,0.05));
+    pname[1] = "defalut";
+    pcolor[1] = evp::Color(1,1,1);
     repopulate();
   }
   void repopulate() {
@@ -81,6 +34,7 @@ public:
        					      pcolor[id]);
       s->onColorIs([this,id](const evp::Color c) {
 	pcolor[id] = c;
+	onUpdate();
       });
 
       // text field
@@ -88,6 +42,7 @@ public:
 		                        30,ypos+25,150,20,it.second);
       ti->onTextIs([id,this](std::string s){
         pname[id] = s;
+	onUpdate();
       });
 
       // delete button
@@ -97,6 +52,7 @@ public:
         pname.erase(id);
         pcolor.erase(id);
         repopulate();
+	onUpdate();
       });
  
       // background
@@ -142,12 +98,104 @@ public:
     pcolor[id] = evp::Color(0,0,0);
 
     repopulate();
+    onUpdate();
   }
+  
+  void onUpdate() {
+    if(onUpdate_) {onUpdate_();}
+  }
+  void onUpdateIs(std::function<void()> f) {onUpdate_=f;}
+  
+  evp::Color paletteColor(int id) {
+    if(pcolor.find(id)!=pcolor.end()) {
+      return pcolor[id];
+    } else {
+      return evp::Color(1,1,1,(float)rand()/(RAND_MAX)*0.2);
+    }
+  }
+
 private:
   std::map<int,std::string> pname;
   std::map<int,evp::Color> pcolor;
   int selectedId_=0;
+  std::function<void()> onUpdate_;
 };
+
+
+class MapArea : public evp::GUI::Area{
+public:
+  MapArea(evp::GUI::Area* const parent,
+	  PaletteArea* paletteArea,
+          const int nPoints,
+	  const float dx,
+	  const float dy
+	 )  : Area("mapArea",parent,0,0,dx,dy),paletteArea_(paletteArea) {
+     vmap = new evp::VoronoiMap<CInfo>(10000,dx,dy);
+     mapInitialize();
+     mapColorize();
+
+     paletteArea_->onUpdateIs([this](){
+       mapColorize();
+     });
+  }
+  void mapInitialize() {
+    // set CInfo for all cells:
+    for(auto &c : vmap->cells) {
+      c.info.paletteId = 1;
+      c.info.colFac = 1 - ((float)rand()/(RAND_MAX))*0.2;
+    }
+  }
+  void mapColorize() {
+    // set colors according to new data
+    for(auto &c : vmap->cells) {
+      c.color = (paletteArea_->paletteColor(c.info.paletteId) * c.info.colFac).toSFML();
+    }
+    vmap->create_mesh();
+  }
+  virtual void draw(const float px,const float py, sf::RenderTarget &target, const float pscale) {
+    float gx = x_+px*pscale;
+    float gy = y_+py*pscale;
+    DrawRect(gx, gy, dx_*pscale, dy_*pscale, target,
+		    evp::HSVToColor((float)rand()/(RAND_MAX),0.5,0.5));
+    vmap->draw(gx,gy,zoom_,target);
+  }
+  void zoomIs(const float z) {
+    zoom_ = std::min(1e2f, std::max(1e-2f,z));
+    sizeIs(vmap->spread_x*zoom_, vmap->spread_y*zoom_);
+    evp::GUI::ScrollArea* a = dynamic_cast<evp::GUI::ScrollArea*>(parent()->parent());
+    a->adjustChildren();// make sure scrollers adjust
+  }
+  virtual void onMouseWheelScrolled(const float dx, const float dy, const float x, const float y) {
+    evp::GUI::ScrollArea* a = dynamic_cast<evp::GUI::ScrollArea*>(parent()->parent());
+    
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl)
+     ||sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RControl)) {
+      float gx = globalX();
+      float gy = globalY();
+      float oldZ = zoom_;
+      zoomIs(pow(1.1,dy) * zoom_);
+      a->adjustChildren();// make sure scrollers adjust
+      
+      float realZF = zoom_/oldZ;
+      a->doScroll(
+		  (x-gx)*(realZF-1),
+		  (y-gy)*(realZF-1)
+		  );// take zoom correction, to warp around mouse
+      
+    } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift)
+     ||sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RShift)) {
+      a->doScroll(-dy*10,-dx*10);// invert scroll axes
+    } else {
+      a->doScroll(-dx*10,-dy*10);
+    }
+  }
+private:
+  evp::VoronoiMap<CInfo>* vmap;
+  float zoom_=1.0;
+  bool hasData_=false;
+  PaletteArea* paletteArea_;
+};
+
 
 class Editor {
 public:
@@ -177,7 +225,7 @@ public:
     scrollp->fillParentIs(false,true,true);
 
     // MapArea in center / bottom-right
-    mapArea = new MapArea(NULL,10000,1000,1000);
+    mapArea = new MapArea(NULL,paletteArea,10000,1000,1000);
     evp::GUI::Area* scroll = new evp::GUI::ScrollArea("scroll",window,mapArea,x+225,y+topBarOffset,dx,dy);
     mapArea->colorIs(evp::Color(1,0,0));
     scroll->fillParentIs(true,true,true);
